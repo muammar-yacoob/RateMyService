6fee6f8e1695d336d0b4091f768bdf5b004031f7
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-01-15 16:11:02 +0000

    routs configured

---
+++ server.js
@@ -0,0 +1,10 @@
+﻿const express = require('express');
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+app.listen(PORT, () =>  console.log(`Server listening on port ${PORT}`));
