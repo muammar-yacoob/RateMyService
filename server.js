434470568497a5d4718cbbf0851eaaa859dac3b2
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-01-15 19:07:00 +0000

    contactController accepts body

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 ﻿const express = require('express');
 const dotenv = require('dotenv').config();
-const mongoose = require('mongoose');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(PORT, () =>  console.log(`Server listening on port ${PORT}`));
