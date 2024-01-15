90f094374923edaf9211fcde54e9769012c93946
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-01-15 20:40:22 +0000

    error handler

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 ﻿const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 
 
@@ -7,5 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(PORT, () =>  console.log(`Server listening on port ${PORT}`));
