272ac2fafb9061f352a09402e093fcb4a36bc571
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-01-16 13:02:14 +0000

    contacts renamed to user

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {
-    getAllContacts,
-    getContact,
-    createContact,
-    updateContact,
-    deleteContact
+    getAllUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
 
 } = require('../controllers/contactController');
 
-router.route('/').get(getAllContacts).post(createContact);
-router.route('/:id', ).get(getContact).put(updateContact).delete(deleteContact);
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id', ).get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
