8a175691cd2f55f1cc2330a7dc4de410b57af19c
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-03-07 22:02:38 +0000

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,14 +2,24 @@ const mongoose = require('mongoose');
 let cachedDb = null;
 
 async function connectToDatabase(uri) {
-    if (cachedDb) {
-      console.log('Using existing database connection');
-      return cachedDb;
-    }
-    console.log('Creating new database connection');
-    const db = await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    cachedDb = db;
-    return db;
+  if (cachedDb && mongoose.connection.readyState === 1) {
+    console.log('Using existing database connection');
+    return cachedDb;
   }
+
+  if (mongoose.connection.readyState === 3) {
+    await mongoose.disconnect();
+  }
+
+  console.log('Creating new database connection');
+  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected!');
+    cachedDb = null;
+  });
+
+  cachedDb = mongoose.connection;
+  return cachedDb;
+}
 
 module.exports = connectToDatabase;
