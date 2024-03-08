60f9824740ed477fba1195edabbf618f9c0e0040
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-03-08 12:55:52 +0000

    background works after changing toml

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -1,7 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Ratings = require('../models/ratingModel');
 const User = require('../models/userModel');
-const getThankyouImage = require('../utils/thankYouImageHelper.js');
 const randIP = require('../utils/generateRandomIpAddress.js');
 const cowsay = require('cowsay');
 
@@ -18,8 +17,13 @@ const serveUserRatingPage = asyncHandler(async (req, res, next) => {
             res.status(500).send(`<pre>${cowMessage}</pre>`);
             return;
         }
+        const encodedUserId = encodeURIComponent(userId);
+        const encodedUserName = encodeURIComponent(user.name);
 
-        res.render('rating-form', { userId: userId, userName: user.name });
+        const redirectUrl = `/views/rating-form.html?userId=${encodedUserId}&userName=${encodedUserName}`;
+        res.redirect(redirectUrl);
+
+        // res.render('rating-form', { userId: userId, userName: user.name });// ejs
     }
         catch (err) {
             const cowMessage = cowsay.say({ text: `Looks like this user has Mooooved!` });
@@ -30,47 +34,51 @@ const serveUserRatingPage = asyncHandler(async (req, res, next) => {
 });
 
 let firstTimePosting = true; 
+
 const postRating = asyncHandler(async (req, res, next) => {
     const userId = req.params.userId;
     const { customerName, rating = 5, comments } = req.body;
     let ipAddress = req.ip || req.socket.remoteAddress;
 
-    //ipAddress = await randIP();
+    // Assuming randIP() is not required as we are using the real IP address now.
     const ratingData = { userId, customerName, rating, ipAddress, comments };
 
-    if (!userId || !customerName || !rating) {
-        throw new Error("Missing data!");
+    if (!userId || !customerName) {
+        return res.status(400).json({ message: "Missing required data!" });
     }
 
-    let hasRated = false;
     let updatedRating;
 
     try {
         if (firstTimePosting) {
             firstTimePosting = false;
-            ipAddress
             const existingRating = await Ratings.findOne({ userId, ipAddress });
-            hasRated = !!existingRating;
+            const hasRated = !!existingRating;
 
-            console.log("Posting Rating...");
-            updatedRating = await Ratings.findOneAndUpdate(
-                { userId, ipAddress },
-                ratingData,
-                { new: true, upsert: true }
-            );
+            if (!hasRated) {
+                console.log("Creating or Updating Rating...");
+                updatedRating = await Ratings.findOneAndUpdate(
+                    { userId, ipAddress },
+                    ratingData,
+                    { new: true, upsert: true }
+                );
+            }
         }
 
-        const ratingScore = updatedRating ? updatedRating.rating : 'N/A';
-        const again = hasRated ? ' again' : '';
-        const thankYouImage = await getThankyouImage();
+        const encodedUserId = encodeURIComponent(userId);
+        const user = await User.findOne({ _id: userId });
+        const encodedUserName = encodeURIComponent(user.name);
 
-        console.log("Rendering thank-you with:", { ratingScore, again });
-        res.render('thank-you', { ratingScore, again, thankYouImage});
+        // Redirect URL updated to be compliant with serving static HTML and passing parameters for client-side JS to use
+        const redirectUrl = `/views/thank-you.html?userId=${encodedUserId}&userName=${encodedUserName}`;
+        return res.redirect(redirectUrl);
 
     } catch (err) {
-        next(err);
+        console.error("Error processing rating:", err);
+        return next(err);
     }
 });
+
 
 //@desc Get all ratings by userId
 //@route GET /api/ratings/:userId
--- routes/homeExampleRoute.js
+++ routes/homeExampleRoute.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 //Home page
-const usage = `<center><br><p><font face=Arial><h2>Welcome to TipEase!</h2>
+// const bgImage = 'https://img.freepik.com/premium-vector/kawaii-chicken-duck-seamless-background_6997-1604.jpg';
+const bgImage = '/res/imgs/bg.jpg';
+const usage = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TipEase</title>
+</head>
+<body style="background-image: url('${bgImage}');">
+<center><br><p><font face=Arial><h2>Welcome to TipEase!</h2>
 <h3>Endpoint Examples:</h3>
 <h4>User APIs:</h4>
 <a href="/api/users">List all users</a><br>
@@ -10,7 +19,10 @@ const usage = `<center><br><p><font face=Arial><h2>Welcome to TipEase!</h2>
 <a href="/api/ratings/65a6feb068f227551d31b2c0">List ratings by User</a><br>
 <h4>Pages</h4>
 <a href="/rate/65a6feb068f227551d31b2c0">Rate a user</a><br>
-<a href="/profile/65a6feb068f227551d31b2c0">User Profile Page</a><br></font></p></center>`;
+<a href="/profile/65a6feb068f227551d31b2c0">User Profile Page</a><br></font></p></center>
+</body>
+</html>
+`;
 router.get('/', (req, res) => res.send(usage));
 
 module.exports = router;
