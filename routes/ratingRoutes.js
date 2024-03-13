b45e206373e566ac6a367f278a47702db4729032
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-03-13 18:17:36 +0000

    Update ratingRoutes.js

--- routes/ratingRoutes.js
+++ routes/ratingRoutes.js
@@ -2,16 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {
     serveUserRatingPage,
-    postRating,
     getRatingsByUserId,
-    deleteRatings
 } = require('../controllers/ratingController');
 
-router.route('/api/ratings/:userId?')
-    .get(getRatingsByUserId)
-    .post(postRating)
-    .delete(deleteRatings);
-
+router.get('/api/ratings/:userId', getRatingsByUserId);
 router.get('/rate/:userId', serveUserRatingPage);
 
 module.exports = router;
