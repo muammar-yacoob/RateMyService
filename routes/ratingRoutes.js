4ad55d6c5504a09af304f54bc375b777f99ffca8
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-03-13 20:42:29 +0000

    posting ratings and thank you page fixed

--- routes/ratingRoutes.js
+++ routes/ratingRoutes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const {
     serveUserRatingPage,
     getRatingsByUserId,
+    postRating
 } = require('../controllers/ratingController');
 
 router.get('/api/ratings/:userId', getRatingsByUserId);
+router.post('/rate/:userId', postRating);
+
+
 router.get('/rate/:userId', serveUserRatingPage);
 
 module.exports = router;
